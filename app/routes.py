64b947f07ffdc90104517aa3eacb6e2f5ecb9dca
Dishank <dishank.sharma.1912@gmail.com>
2026-06-13 00:50:31 +0530

    Implemented RBAC using Flask Blueprint.

---
+++ app/routes.py
@@ -0,0 +1,40 @@
+from flask import Blueprint, request, render_template, session, redirect, url_for, flash
+from werkzeug.security import check_password_hash
+from app.models import User
+
+authentication_bp = Blueprint('auth', __name__)
+
+@authentication_bp.route('/login', methods=['GET', 'POST'])
+def login():
+
+    if request.method == 'GET': # If user is just sitting on Login Page or clicked to view Login page
+        return render_template('login_test.html')
+
+    # If user has clicked on 'Submit/Login' then its a post request. Thus we will get user's username and password from that POST request 
+
+    username = request.form.get('username')
+    password = request.form.get('password')
+
+    # Now checking if user actually exist in User table or not 
+
+    user = User.query.filter_by(username=username).first()
+    pass_check = check_password_hash(user.password_hash, password)
+
+    if user and pass_check:
+
+        # User exist in table thus we establish connection 
+
+        session['user_id'] = user.id
+        session['role'] = user.role
+
+        if user.role == 'Admin':
+            return redirect(url_for('admin_routes.dashboard'))
+        
+        elif user.role == 'Staff':
+            return redirect(url_for('staff_routes.dashboard'))
+        
+        elif user.role == 'Trekker':
+            return redirect(url_for('trekker_routes.dashboard'))
+
+    flash('Invalid username or Password. Please enter valid credentials')
+    return redirect(url_for('auth.login'))
